e7d8cd71503c8e5658c73bbeac0ef3a546b824bd
Anil tripathi <aniltripathi@Anils-MacBook-Pro-2.local>
2025-03-10 22:31:32 +0530

    updates

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -23,18 +23,18 @@ const Contact = () => {
     <section id="contact" className="py-20 bg-gray-100">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold mb-12 text-center">Get in Touch</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
           <ContactItem
             icon={MapPin}
             title="Location"
             content="Based in the vibrant tech hub of Hyderabad, India. Open to remote opportunities worldwide."
           />
-          <ContactItem
+          {/* <ContactItem
             icon={Phone}
             title="Phone"
             content="Available for calls during Indian business hours. Feel free to schedule a chat!"
             directContact="+91 9642429550"
-          />
+          /> */}
           <ContactItem
             icon={Mail}
             title="Email"
